7884706b0e6edfc2cbd306173bfb4f923fb1f799
niitish <71326553+niitish@users.noreply.github.com>
2023-04-06 01:14:16 +0530

    style and js revamp

--- README.md
+++
@@ -1,5 +0,0 @@
-# dogs-gallery
-
-Fetch random dog pictures with just a click.
-
-See it live [here](https://iseriouslyhate.tech/dogs-gallery).
--- dog.js
+++ dog.js
@@ -1,27 +1,66 @@
-const dog = document.querySelector(".dogs");
-var selectedBreed = document.querySelector(".breed-selector");
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+const drop = document.querySelector(".breed-selector");
+
+const dogs = document.querySelector(".dogs");
+const selectedBreed = document.querySelector(".breed-selector");
+
+var count = 0;
+
+const loader = document.createElement("div");
+loader.className = "loader";
+
+function createList() {
+	fetch(BREEDS_URL)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (processedResponse) {
+			let breedsList = Object.keys(processedResponse.message);
+			breedsList.forEach((breed) => {
+				let html = `<option value=${breed}>${breed.toUpperCase()}</option> `;
+				drop.innerHTML += html;
+			});
+		});
+}
+
+createList();
 
 function createDogs(breed) {
 	var url = "";
 	if (breed === " ") url = "https://dog.ceo/api/breeds/image/random";
 	else url = `https://dog.ceo/api/breed/${breed}/images/random`;
-	const promise = fetch(url);
-	promise
+	fetch(url)
 		.then(function (response) {
 			const processingPromise = response.json();
 			return processingPromise;
 		})
 		.then(function (processedResponse) {
+			const div = document.createElement("div");
+			div.className = "dog";
+			div.appendChild(loader);
+
+			dogs.appendChild(div);
+
+			div.scrollIntoView({ behavior: "smooth" });
+
 			const img = document.createElement("img");
-			console.log(processedResponse.message);
+			img.id = `dog-${count}`;
 			img.src = processedResponse.message;
-			img.style.cssText =
-				"width:90%;margin:10px;padding: 5px;border:2px solid brown;";
-			dog.appendChild(img);
+			img.onclick = function () {
+				window.open(processedResponse.message, "_blank");
+			};
+
+			img.onload = function () {
+				div.removeChild(loader);
+				div.appendChild(img);
+			};
+
+			++count;
 		});
 }
 
 document.querySelector(".create").addEventListener("click", () => {
+	console.log(selectedBreed.value);
 	if (!selectedBreed.value) createDogs(" ");
 	else createDogs(selectedBreed.value);
 });
